786e429829bf3b5cd713b8a96dccae94cafc04f6
valdezax2 <61360602+valdezax2@users.noreply.github.com>
2024-03-19 08:41:51 -0400

    Create initialize.sh

---
+++ initialize.sh
@@ -0,0 +1,32 @@
+#update system
+sudo yum update
+sudo yum install httpd -y
+
+#start http server daemon and run at startup
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+#install php
+sudo yum install php -y
+sudo yum install php-mysqli -y
+
+#restart http server daemon
+sudo systemctl restart httpd
+
+#install wordpress
+cd /var/www/html
+sudo wget http://wordpress.org/latest.tar.gz
+sudo tar -xzvf latest.tar.gz
+
+#copy wordpress config file
+cd wordpress
+sudo cp wp-config-sample.php wp-config.php
+
+
+#install mariadb database
+sudo yum install mariadb105-server.x86_64
+sudo systecmctl start mariadb
+sudo mysqladmin -u root create blog
+
+
+
